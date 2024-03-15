9fb392381d1b9e8cfeb4d755008fe04391d1e8fd
Esther Kubania <122841197+dev-EstherKubania@users.noreply.github.com>
2024-03-15 17:01:15 +0300

    Create 3-to_str.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Module to demonstrate type annotations in Python
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Returns the string representation of a float.
+
+    Args:
+        n (float): The float number.
+
+    Returns:
+        str: The string representation of the float.
+    """
+    return str(n)
